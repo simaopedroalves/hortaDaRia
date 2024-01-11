ff499fc92e2108007250038a6487c1338a5b234c
Simão Pedro <simaaoweb@gmail.com>
2024-01-11 01:52:23 +0000

    add some style to navbar, bottomMenu and fix bugs

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-const openMenu = document.querySelector('.fa-bars-staggered');
-const closeMenu = document.querySelector('.fa-x');
-const menu = document.querySelector('.menu-list')
-
-function showMenu() {
-    menu.style.display = 'grid';
-    openMenu.style.display = 'none';
-    closeMenu.style.display = 'grid';
-}
-
-function desappearMenu() {
-    menu.style.display = 'none';
-    openMenu.style.display = 'grid';
-    closeMenu.style.display = 'none';
-}
-
-// closeMenu.addEventListener('click', () => {
-//     // desappearMenu()
-// })
- openMenu.addEventListener('click', () => {
-     // showMenu()
-     menu.classList.toggle('show')
- })
+ const openMenu = document.querySelector('.openMenu');
+ const menu = document.querySelector('.menu-list')
+// OPEN AND CLOSE THE MENU 
+openMenu.addEventListener('click', () => {
+    menu.classList.toggle('show')
+    openMenu.classList.toggle('fa-x')    
+})
