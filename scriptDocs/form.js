ebe39f8012c2eebf96bb89f24c7aab2da5ab1eab
Simão Pedro <simaaoweb@gmail.com>
2024-03-02 02:05:24 +0000

    Add a constact form page and change some style
    i've been change a display of the productsList.
    I created a new Page called form.html whrw the client can ask questions to the elements of Horta da Ria.

---
+++ scriptDocs/form.js
@@ -0,0 +1,15 @@
+const autoResponseMessage = document.querySelector('#autoResponseMessage');
+const email = document.querySelector('email');
+const clientMessage = document.querySelector('#clientMessage');
+const submitBtn = document.querySelector('#submitBtn');
+const clientName = document.querySelector('#name');
+const subjectNewMessage = document.querySelector('#newMessage')
+
+// RESPOSTA AUTOMATICA DO FORMSUBMIT PARA O CLIENTE
+submitBtn.addEventListener('click', () => {
+    let message = "Obrigado pela tua mensagem! Responder-te-emos o mais breve possível. " +
+    "Eis a tua mensagem: ";
+    
+    autoResponseMessage.value = message + clientMessage.value;
+    subjectNewMessage.value = "Mensagem de " + clientName.value
+})
