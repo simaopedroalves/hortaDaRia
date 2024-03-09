98392e706a635d3e0048dda82c59b9f3cb4a034d
Simão Pedro <simaaoweb@gmail.com>
2024-03-09 01:31:59 +0000

    testing form

--- scriptDocs/form.js
+++ scriptDocs/form.js
@@ -6,10 +6,10 @@ const clientName = document.querySelector('#name');
 const subjectNewMessage = document.querySelector('#newMessage')
 
 // RESPOSTA AUTOMATICA DO FORMSUBMIT PARA O CLIENTE
-submitBtn.addEventListener('click', () => {
-    let message = "Obrigado pela tua mensagem! Responder-te-emos o mais breve possível. " +
-    "Eis a tua mensagem: ";
+// submitBtn.addEventListener('click', () => {
+//     let message = "Obrigado pela tua mensagem! Responder-te-emos o mais breve possível. " +
+//     "Eis a tua mensagem: ";
     
-    autoResponseMessage.value = message + clientMessage.value;
-    subjectNewMessage.value = "Mensagem de " + clientName.value
-})
+//     autoResponseMessage.value = message + clientMessage.value;
+//     subjectNewMessage.value = "Mensagem de " + clientName.value
+// })
