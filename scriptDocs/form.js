8e61f86c22a4954cc46075d9b40031502ff93b36
Simão Pedro <simaaoweb@gmail.com>
2024-04-03 02:25:33 +0100

    chaged descriptions and meta keywords

--- scriptDocs/form.js
+++ scriptDocs/form.js
@@ -1,15 +1,40 @@
 const autoResponseMessage = document.querySelector('#autoResponseMessage');
-const email = document.querySelector('email');
+const email = document.querySelector('#email');
 const clientMessage = document.querySelector('#clientMessage');
 const submitBtn = document.querySelector('#submitBtn');
 const clientName = document.querySelector('#name');
 const subjectNewMessage = document.querySelector('#newMessage')
 
-// RESPOSTA AUTOMATICA DO FORMSUBMIT PARA O CLIENTE
-// submitBtn.addEventListener('click', () => {
-//     let message = "Obrigado pela tua mensagem! Responder-te-emos o mais breve possível. " +
-//     "Eis a tua mensagem: ";
-    
-//     autoResponseMessage.value = message + clientMessage.value;
-//     subjectNewMessage.value = "Mensagem de " + clientName.value
+
+// RESPOSTA AUTOMATICA DO NETLIFY PARA O CLIENTE
+
+  //submitBtn.addEventListener('click', () => {
+
+    // let message = `Obrigado pela tua mensagem ${clientName.value}! \n\nResponder-te-emos o mais breve possível. \n\nEis a tua mensagem: 
+    //                 \n\n${clientMessage.value} 
+    //             `
+    // console.log(message)  
+
+    // autoResponseMessage.value = message + clientMessage.value;
+    //      subjectNewMessage.value = "Mensagem de " + clientName.value
+
+         
+
 // })
+
+
+function emailSend() {
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "simaaoweb@gmail.com",
+        Password : "E45C3E9DC2EA7C380F415DF4252704AEB859",
+        To : email.value,
+        From : "simaaoweb@gmail.com",
+        Subject : "This is the subject",
+        Body : clientMessage.value
+    }).then(
+    message => alert(message)
+    );
+
+}
