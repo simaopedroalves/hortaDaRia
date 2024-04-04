b85251ba3e88fc395c93d96c75e8b97122012e6b
Simão Pedro <simaaoweb@gmail.com>
2024-04-04 01:19:47 +0100

    update success.html and bottom Menu

--- scriptDocs/form.js
+++ scriptDocs/form.js
@@ -4,19 +4,11 @@ const clientMessage = document.querySelector('#clientMessage');
 const submitBtn = document.querySelector('#submitBtn');
 const clientName = document.querySelector('#name');
 const subjectNewMessage = document.querySelector('#newMessage')
-const divThankYouMessage = document.querySelector('.thankYouMessage')
 
 
 // RESPOSTA AUTOMATICA DO NETLIFY PARA O CLIENTE
 
 submitBtn.addEventListener('click', () => {
-
-    let message =   `
-                        <span>Obrigado ${clientName}!</span>\n 
-                        A tua mensagem foi enviada com sucesso.\n 
-                    `;
-
-     divThankYouMessage += message;
 
     subjectNewMessage.value = "Mensagem de " + clientName.value;
 })
