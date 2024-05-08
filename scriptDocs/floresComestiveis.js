6097945e722a6ea35e7734b20970926e274a6171
Simão Pedro <simaaoweb@gmail.com>
2024-05-08 02:23:39 +0100

    added id to each product and more...
    added animation to navbar list;
    trying to figure out how can i add each item to cart... push men, push xD

--- scriptDocs/floresComestiveis.js
+++ scriptDocs/floresComestiveis.js
@@ -1,27 +1,37 @@
 const secfloresComestiveis = document.querySelector('.floresComestiveis');
 const cartList = document.querySelector('.shopping-cart')
+const cart = [];
+
 
 async function callFloresComestiveis () {
+
     return (await fetch('/productsList.json')).json()
+   
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-
+ 
     let object = '';
 
     try {
         object = await callFloresComestiveis();
+        
     }
     catch (error) {
         console.error('ERROR');
         console.log(error)
     }
 
-    for (let i = 0; i < object.verduras.length; i++) {
+
+    
+
+    for (let i = 0; i < object.floresComestiveis.length; i++) {
         
         let floresComestiveisName = object.floresComestiveis[i].name;
         let floresComestiveisPrice = object.floresComestiveis[i].price;
         let image = object.floresComestiveis[i].image;
+        let product_id = object.floresComestiveis[i].productId;
+
 
         //WHEN, IN productList.json() AN IMAGE KEY IS AN EMPTY STRING
         if (image == '') {
@@ -32,7 +42,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         <div class="boxItem">
             <h3 id="itName">${floresComestiveisName}</h3>
             <img src="${image}" alt="">
-            <div class="kiloPrice">${floresComestiveisPrice}<span>€/kilo</span></div>
+            <div class="kiloPrice">${floresComestiveisPrice}€/Un</div>
             <select type="text" min="1" class="quantity" placeholder="quantidade">
                 <!-- <option value="">Quantidade</option> -->
                 <option value="1Un">1 Un</option>
@@ -46,7 +56,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                 <option value="500Un">500 Un</option>
             </select>
             <!-- Igual à quantidade a multiplicar pelo preço por kilo -->
-            <div class="priceToPay">Total<span>€</span></div>
+            <div class="priceToPay"></div>
             <button class="addToCart btn btn-success">Comprar</button>
         </div>
         `
@@ -54,45 +64,76 @@ document.addEventListener('DOMContentLoaded', async () => {
         let addCartBtn = document.querySelectorAll('.addToCart');
         let titleName = document.querySelectorAll('#itName');
         let selectedOptionValue = document.querySelectorAll('.quantity');
+        let kiloPrice = document.querySelectorAll('.kiloPrice');
+        let finalItemPrice = document.querySelectorAll('.priceToPay') 
 
-        
-        function addThisItemToCart(imagem, titulo) {
-            let newItem = document.createElement('div')
+                // cartList.appendChild(document.createElement('div'));
+      
 
-            newItem.innerHTML = `
-                    <div class="box-1">
-                        <img src="${imagem}" alt="">
-                    </div>
-                        
-                    <div class="box-2">
+        selectedOptionValue.forEach((btn, i) => {
 
-                        <h3>${titulo}</h3>
+            btn.addEventListener('click', () => {           
 
-                        <div class="box-2-child">
-                            <button class="lessOneItemBtn">-</button>
-                            <input type="number" >
-                            <button class="oneMoreItemBtn">+</button>
-                        </div>
+                function finalPricePerItem (kg, qt) {
+                    var priceToPay = 0;
+                    kg = parseFloat(selectedOptionValue[i].value);
+                    qt = parseFloat(kiloPrice[i].textContent)
+                    priceToPay = kg * qt
+                    finalItemPrice[i].textContent = priceToPay + ' €'
+                    return priceToPay
 
-                        <div>
-                            <span class="priceToPay">Total</span>
-                            <button><i class="fa-regular fa-trash-can"></i></button>
-                        </div>
-                    </div>
-                `
-                cartList.appendChild(document.createElement('div'));
-                newItem.classList.add('box-cart');
-        }
+                }
 
-        addCartBtn.forEach((btn, i) => {
-            btn.addEventListener('click', () => {
-                console.log('Quero ' + selectedOptionValue[i].value + ' de ' + titleName[i].textContent)
+                finalPricePerItem() 
 
-                addThisItemToCart(`${image}`, `${floresComestiveisName}`)
+                // function addCartToHTML () { 
+                
+                //     let newItem = document.createElement('div')
+                //     let totalPrice = document.querySelectorAll('.priceToPay')
+
+                //     newItem.classList.add('box-cart');
+                //      cartList.textContent = '';
+                //      cartList.appendChild(newItem) 
+
+                //     newItem.innerHTML = `
+                    
+                //         <div class="box-1">
+                //             <img src="${image}" alt="">
+                //         </div>
+                            
+                //         <div class="box-2">
+
+                //             <h3>${floresComestiveisName}</h3>
+
+                //             <div class="box-2-child">
+                //                 <button class="lessOneItemBtn">-</button>
+                //                 <input type="number" >
+                //                 <button class="oneMoreItemBtn">+</button>
+                //             </div>
+
+                //             <div>
+                //                 <span class="priceToPay">${totalPrice}</span>
+                //                 <button><i class="fa-regular fa-trash-can"></i></button>
+                //             </div>
+                //         </div>
+                //     `
+                //   console.log(newItem)
+                // }
+
             })
+            
         })
 
+// aqui o objetivo é adicionar o item ao carrinho com outro stylesheet ja defenido
+        
+
+            // para ser adicionado à memoria
+            // function addCartToLocalStorage() {
+            //     localStorage.setItem('cart', JSON.stringify(cart))
+            // }
+            
+        
+
     }
-    
 })
 
