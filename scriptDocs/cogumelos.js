7ea9ed9cfd42b28183582866c79c30daab102513
simaopedroalves <simaaoweb@gmail.com>
2024-10-30 03:20:52 +0000

    Added 2 new sections/familys
    added compotas and cogumelos html and js files.
    
    fix some prices and change title names

---
+++ scriptDocs/cogumelos.js
@@ -0,0 +1,177 @@
+const secCogumelos = document.querySelector('.cogumelos');
+
+async function callCogumelos () {
+    return (await fetch('/productsList.json')).json()
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    let object = '';
+
+    try {
+        object = await callCogumelos();
+    }
+    catch (error) {
+        console.error('ERROR');
+        console.log(error)
+    }
+
+    for (let i = 0; i < object.cogumelos.length; i++) {
+        
+        let cogumelosName = object.cogumelos[i].name;
+        let cogumelosPrice = object.cogumelos[i].price;
+        let image = object.cogumelos[i].image;
+        let stock = object.cogumelos[i].stock;
+        let product_id = object.cogumelos[i].productId;
+
+        //WHEN, IN productList.json() AN IMAGE KEY IS AN EMPTY STRING
+        if (image == '') {
+            image = "/images/logo.png";
+        }
+        
+        secCogumelos.innerHTML += `
+            <div class="boxItem" id="${product_id}">
+                <h3 id="itName">${cogumelosName}</h3>
+                <img src="${image}" alt="">
+                <div class="kiloPrice">${cogumelosPrice}€/Kg</div>
+                <select type="text" min="1" class="quantity" placeholder="quantidade">
+                    <option value="qt">Quantidade</option>
+                    <option value="15gr">15 gr.</option>
+                    <option value="25gr">25 gr.</option>
+                    <option value="50gr">50 gr.</option>
+                    <option value="100gr">100 gr.</option>
+                    <option value="250gr">250 gr.</option>
+                    <option value="500gr">500 gr.</option>
+                </select>
+                <!-- Igual à quantidade a multiplicar pelo preço por kilo -->
+                <div class="priceToPay"></div>
+                <button class="addToCart btn btn-success" disabled>Comprar</button>
+            </div>
+        `
+
+        // function on script.js
+        findStockOfItems (stock, product_id)
+
+        
+        let addCartBtn = document.querySelectorAll('.addToCart');
+
+        function addItem() {
+
+            addCartBtn.forEach(btn => {
+                btn.addEventListener('click', (event) => {
+                    btn = event.target;
+                    let name = btn.parentElement.querySelector('#itName').textContent;
+                    let imageSrc = btn.parentElement.querySelector('img').src;
+                    let itemPrice = btn.parentElement.querySelector('.kiloPrice').textContent;
+                    let quantity = btn.parentElement.querySelector('.quantity').value;
+                    let itemTotal = btn.parentElement.querySelector('.priceToPay').textContent;
+                    addToitemObj(name, imageSrc, itemPrice, quantity, itemTotal)
+                    updateNumbItemsOnCart()
+                    refreshItemSelected(btn)
+                    showAllert(name)
+                    btn.setAttribute("disabled", "")
+                })
+            })
+        }
+
+        addItem()
+
+        function refreshItemSelected (btn) {
+            btn.parentElement.querySelector('.priceToPay').textContent = '';
+        }
+
+        function showAllert (name) {
+            let alert = document.querySelector('.alert');
+            alert.classList.add('show-alert');
+
+            alert.innerHTML = `
+                <span class="cart-changed-message">${name} adicionado(a) ao Cesto</span>
+                <button class="see-cart">
+                    <a href="/html/carrinho.html">
+                        Ver Carrinho
+                    </a> 
+                </button>
+            `
+
+            setTimeout(() => {
+                alert.classList.remove('show-alert')
+            }, 2000);
+        }
+    
+        function addToitemObj(name, imageSrc, itemPrice, quantity, itemTotal) {
+            let itemObj = JSON.parse(localStorage.getItem('cart'))
+           
+            if (itemObj === null) {
+                itemObj = []
+            }
+
+            itemObj.push({
+                itName: name,
+                itImageSrc: imageSrc,
+                itPrice: itemPrice,
+                itQuantity: quantity,
+                itTotal: itemTotal
+            })
+            localStorage.setItem('cart', JSON.stringify(itemObj))
+        }
+                    
+        let selectedOptionValue = document.querySelectorAll('.quantity');
+        let kiloPrice = document.querySelectorAll('.kiloPrice');
+        let finalItemPrice = document.querySelectorAll('.priceToPay'); 
+
+        selectedOptionValue.forEach((btn, i) => {
+
+            btn.addEventListener('change', () => {           
+
+                function finalPricePerItem (kg, qt) {
+                    var priceToPay = 0;
+                    kg = parseFloat(selectedOptionValue[i].value);
+                    qt = parseFloat(kiloPrice[i].textContent);
+
+                    let qtText = selectedOptionValue[i].value;
+                    let addCartBtn = btn.parentElement.querySelector('.addToCart')
+
+                    if (qtText === "qt") {
+                        finalItemPrice[i].textContent = ''
+                        addCartBtn.setAttribute("disabled", "")
+                    }
+                    else {
+                    priceToPay = kg * (qt/1000)
+                    priceToPay = priceToPay.toFixed(2)
+                    finalItemPrice[i].textContent = priceToPay + ' €'
+                    addCartBtn.removeAttribute("disabled")
+                    }
+                    
+                    return priceToPay
+
+                }
+
+                let buyBtn = btn.parentElement.querySelector('.addToCart');
+                buyBtn.removeAttribute('disabled'); 
+
+                finalPricePerItem() 
+            })
+            
+        })
+
+    }
+    updateNumbItemsOnCart()
+})
+
+function updateNumbItemsOnCart() {
+    let numbOfItemsOnCart = document.querySelectorAll('nav .article-number');
+    let cart = JSON.parse(localStorage.getItem('cart'));
+
+    numbOfItemsOnCart.forEach(el => {
+        el.textContent = '0'
+
+        for (let i = 0; i < cart.length; i++) {
+            if (cart.length > 0) {
+                el.textContent = cart.length
+            }
+            if (cart.length <= 0) {
+                el.textContent = '0'
+            }
+        }
+    })
+}
--- scriptDocs/microverdes.js
+++ scriptDocs/microverdes.js
@@ -19,42 +19,72 @@ document.addEventListener('DOMContentLoaded', async () => {
     } 
 
     for (let i = 0; i < object.microverdes.length; i++) {
-
+        
         let microverdesName = object.microverdes[i].name;
         let microverdesPrice = object.microverdes[i].price;
         let image = object.microverdes[i].image;
         let stock = object.microverdes[i].stock;
         let product_id = object.microverdes[i].productId;
+        let vasos = microverdesName.split(' ')[1];
+        console.log(vasos);
         
         //WHEN, IN productList.json() AN IMAGE KEY IS AN EMPTY STRING
         if (image == '') {
             image = "/images/logo.png";
         }
 
-        secMicroverdes.innerHTML += `
-        <div class="boxItem" id="${product_id}">
-            <h3 id="itName">${microverdesName}</h3>
-            <img src="${image}" alt="">
-            <div class="kiloPrice">${microverdesPrice}€/Kg</div>
-            <select type="text" min="1" class="quantity" placeholder="quantidade">
-                <option value="qt">Quantidade</option>
-                <option value="25gr">25 gr</option>
-                <option value="50gr">50 gr</option>
-                <option value="75gr">75 gr</option>
-                <option value="100gr">100 gr</option>
-                <option value="150gr">150 gr</option>
-                <option value="200gr">200 gr</option>
-                <option value="250gr">250 gr</option>
-                <option value="500gr">500 gr</option>
-                <option value="1Un">1 Un</option>
-                <option value="2Un">2 Un</option>
-                <option value="3Un">3 Un</option>
-            </select>
-            <!-- Igual à quantidade a multiplicar pelo preço por kilo -->
-            <div class="priceToPay"></div>
-            <button class="addToCart btn btn-success" disabled>Comprar</button>
-        </div>
-        `
+        // if (vasos != 'vasos') {
+
+            secMicroverdes.innerHTML += `
+            <div class="boxItem" id="${product_id}">
+                <h3 id="itName">${microverdesName}</h3>
+                <img src="${image}" alt="">
+                <div class="kiloPrice">${microverdesPrice}€/Kg</div>
+                <select type="text" min="1" class="quantity" placeholder="quantidade">
+                    <option value="qt">Quantidade</option>
+                    <option value="25gr">25 gr</option>
+                    <option value="50gr">50 gr</option>
+                    <option value="75gr">75 gr</option>
+                    <option value="100gr">100 gr</option>
+                    <option value="150gr">150 gr</option>
+                    <option value="200gr">200 gr</option>
+                    <option value="250gr">250 gr</option>
+                    <option value="500gr">500 gr</option>
+                    <option value="1Un">1 Un</option>
+                    <option value="2Un">2 Un</option>
+                    <option value="3Un">3 Un</option>
+                </select>
+                <!-- Igual à quantidade a multiplicar pelo preço por kilo -->
+                <div class="priceToPay"></div>
+                <button class="addToCart btn btn-success" disabled>Comprar</button>
+            </div>
+            `
+        // }
+
+        // secMicroverdes.innerHTML += `
+        // <div class="boxItem" id="${product_id}">
+        //     <h3 id="itName">${microverdesName}</h3>
+        //     <img src="${image}" alt="">
+        //     <div class="kiloPrice">${microverdesPrice}€/Kg</div>
+        //     <select type="text" min="1" class="quantity" placeholder="quantidade">
+        //         <option value="qt">Quantidade</option>
+        //         <option value="25gr">25 gr</option>
+        //         <option value="50gr">50 gr</option>
+        //         <option value="75gr">75 gr</option>
+        //         <option value="100gr">100 gr</option>
+        //         <option value="150gr">150 gr</option>
+        //         <option value="200gr">200 gr</option>
+        //         <option value="250gr">250 gr</option>
+        //         <option value="500gr">500 gr</option>
+        //         <option value="1Un">1 Un</option>
+        //         <option value="2Un">2 Un</option>
+        //         <option value="3Un">3 Un</option>
+        //     </select>
+        //     <!-- Igual à quantidade a multiplicar pelo preço por kilo -->
+        //     <div class="priceToPay"></div>
+        //     <button class="addToCart btn btn-success" disabled>Comprar</button>
+        // </div>
+        // `
 
         // function on script.js
         findStockOfItems (stock, product_id)
@@ -163,7 +193,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             })
             
         })
-        lookForVasos(microverdesName, microverdesPrice, product_id, image)
+        lookForVasos(microverdesName, microverdesPrice, product_id, image, vasos)
     }
 
     updateNumbItemsOnCart()
@@ -188,20 +218,37 @@ function updateNumbItemsOnCart() {
     })
 }
 
-function lookForVasos(microverdesName, microverdesPrice, product_id, image) {
-    microverdesName = [...microverdesName] 
-   let str = microverdesName.join('').toString()
+function lookForVasos(microverdesName, microverdesPrice, product_id, image, vasos) {
 
-    console.log(microverdesName);
-    console.log(str);
-    console.log(typeof(str));
-
-    let strArr = str.split(' ')
-    console.log(strArr[1]);
+    // let vasos = microverdesName.split(' ')[1];
     
-    if (strArr[1] === 'vasos') {
-        console.log('match');
+    // if (vasos == 'vasos') {
+
+    //     secMicroverdes.innerHTML +=  `
+    //          <div class="boxItem" id="${product_id}">
+    //             <h3 id="itName">${microverdesName}</h3>
+    //             <img src="${image}" alt="">
+    //             <div class="kiloPrice">${microverdesPrice}€/Kg</div>
+    //             <select type="text" min="1" class="quantity" placeholder="quantidade">
+    //                 <option value="qt">Quantidade</option>
+    //                 <option value="suculentaEAmorPerfeito">Suculenta e Amor-perfeito</option>
+    //                 <option value="50gr">2 Vasos</option>
+    //                 <option value="75gr">3 Vasos</option>
+    //                 <option value="100gr">100 gr</option>
+    //                 <option value="150gr">150 gr</option>
+    //                 <option value="200gr">200 gr</option>
+    //                 <option value="250gr">250 gr</option>
+    //                 <option value="500gr">500 gr</option>
+    //                 <option value="1Un">1 Un</option>
+    //                 <option value="2Un">2 Un</option>
+    //                 <option value="3Un">3 Un</option>
+    //             </select>
+    //             <!-- Igual à quantidade a multiplicar pelo preço por kilo -->
+    //             <div class="priceToPay"></div>
+    //             <button class="addToCart btn btn-success" disabled>Comprar</button>
+    //         </div>
+    //     `
         
-    }
+    // }
     
 }
