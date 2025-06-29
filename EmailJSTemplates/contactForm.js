3b8c3f6fe5d48b9a0eece24a9c27b01c4ed1b762
simaopedroalves <simaaoweb@gmail.com>
2025-06-30 00:44:51 +0100

    Bug in Email JS Templates
    commented all Email Js code for review.
    
    Messages are submited by Netlify forms again

--- EmailJSTemplates/contactForm.js
+++ EmailJSTemplates/contactForm.js
@@ -1,35 +1,35 @@
-function sendMail() {
-    let clientData = {
-        //dados do cliente e formulario de entrega
-        nome: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        telemovel: document.getElementById("phoneNumber").value,
-        mensagem: document.getElementById("clientMessage").value,
-    }
-    // emailjs.send('Email Service', 'Template ID', function with data)
-    emailjs.send('service_3u285ss', 'contactFormHortaDaRia', clientData) 
-    .then(function() {
-        // alert("Email sent successfully!");
-    }, function(error) {
-        console.log('FAILED...', error);
-        alert("De momento não é possível enviar a tua mensagem. Por favor, tenta mais tarde. Obrigado!");
-    });
-    console.log(clientData);
+// function sendMail() {
+//     let clientData = {
+//         //dados do cliente e formulario de entrega
+//         nome: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         telemovel: document.getElementById("phoneNumber").value,
+//         mensagem: document.getElementById("clientMessage").value,
+//     }
+//     // emailjs.send('Email Service', 'Template ID', function with data)
+//     emailjs.send('service_3u285ss', 'contactFormHortaDaRia', clientData) 
+//     .then(function() {
+//         // alert("Email sent successfully!");
+//     }, function(error) {
+//         console.log('FAILED...', error);
+//         alert("De momento não é possível enviar a tua mensagem. Por favor, tenta mais tarde. Obrigado!");
+//     });
+//     console.log(clientData);
 
-}
+// }
 
-function openSuccessPage(event) {
-    event.preventDefault(); // Prevent the default form submission
- setTimeout(function() {
-    // Redirect to the success page
-    window.location.href = "/html/success.html";
- }, 1000)}; 
+// function openSuccessPage(event) {
+//     event.preventDefault(); // Prevent the default form submission
+//  setTimeout(function() {
+//     // Redirect to the success page
+//     window.location.href = "/html/success.html";
+//  }, 1000)}; 
 
-const submitButton = document.getElementById("submitBtn");
-submitButton.addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    sendMail();
+// const submitButton = document.getElementById("submitBtn");
+// submitButton.addEventListener("click", function(event) {
+//     event.preventDefault(); // Prevent the default form submission
+//     sendMail();
 
-    openSuccessPage(event);
-})
+//     openSuccessPage(event);
+// })
 
